3f5755d09081fe923d4c45b068d99bba4873b177
Chocofrito <zyriab@gmail.com>
2021-05-27 10:39:38 +0200

    start

---
+++ ClassesBase/Personnage.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace ClassesBase
+{
+    public class Personnage
+    {
+        public int _force;
+        public int _stamina;
+
+        public int Force
+        {
+            get
+            {
+                return _force;
+            }
+            set
+            {
+                _force = value;
+            }
+        }
+
+        public int Stamina
+        {
+            get
+            {
+                return _stamina;
+            }
+            set
+            {
+                _stamina = value;
+            }
+        }
+
+        public void seDeplacer (){
+            Console.WriteLine ("Je bouge!");
+        }
+        public void attaquer (){
+            Console.WriteLine ("J'attaque!!");
+        }
+        public string obtenirDescription (){
+            string description = "Je suis un personnage d'un jeu";
+            return (description);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ClassesBase;
+
+namespace UML_Games_Cours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wads!");
+            Personnage perso1 = new Personnage();
+            //Personnage perso2 = new Personnage();
+            perso1.seDeplacer();
+            perso1.attaquer();
+            perso1.Force = 50; // appel au set
+            Console.WriteLine ("La force de perso1 est: " + perso1.Force); // appel au get
+            perso1.Force += 25;
+            Console.WriteLine ("La force de perso1 est: " + perso1.Force); // appel au get
+            Console.WriteLine(perso1.obtenirDescription());
+            string msgDescription = perso1.obtenirDescription();
+            Console.WriteLine (msgDescription);
+        }
+    }
+}
+
+
+// res = somme (4,5)
+// write (res)
+
+// write (somme (4,5))
